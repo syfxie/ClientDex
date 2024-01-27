74feac60b1030ef80c6bfa25be3ea368919869ab
sophiexie <sophiexie118@gmail.com>
2024-01-27 01:31:55 -0500

    Set up server

---
+++ server/app.py
@@ -0,0 +1,68 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/add_contact', methods=['POST'])
+def create_contact():
+    data = request.get_json()
+    app.logger.info(data)
+
+    # add to database
+
+    response = {'message': 'Contact added successfully'}
+    return jsonify(response), 201
+
+
+# Index and filter contacts
+@app.route('/', methods=['GET'])
+def list_contacts():
+    data = request.get_json()
+
+    # add to database
+
+    response = {'message': 'Contact added successfully'}
+    return jsonify(response), 201
+
+
+# Show contact by ID
+@app.route('/contact', methods=['GET'])
+def show_contact():
+    contact_id = request.args.get('id')
+
+    if not (contact_id and len(contact_id) > 0):
+        return jsonify({'error': 'Invalid contact ID'}), 400
+
+    # retrieve from database
+    contact = ['Sophie', 'Xie', ['Blacklisted'], 'Company', 'Toronto', 'sophie@gmail.com',
+               '1234567890', 'notes notes notes', '02-02-2020']
+
+    if contact:
+        keys = ['first_name', 'last_name', 'labels', 'company', 'location',
+                'email', 'phone_number', 'notes', 'last_contacted']
+        data = dict(zip(keys, contact))
+        return jsonify(data), 200
+    else:
+        return jsonify({'error': 'Contact not found'}), 404
+
+
+@app.route('/update_contact', methods=['GET'])
+def update_contact():
+    data = request.get_json()
+
+    contact_id = data['id']
+
+    if not (contact_id and len(contact_id) > 0):
+        return jsonify({'error': 'Invalid contact ID'}), 400
+
+    # edit database
+    contact = ['Sophie', 'Xie', ['Potential Customer'], 'Company', 'Toronto', 'sophie@gmail.com',
+               '1234567890', 'notes notes notes', '02-02-2020']
+
+    if contact:
+        keys = ['first_name', 'last_name', 'labels', 'company', 'location',
+                'email', 'phone_number', 'notes', 'last_contacted']
+        data = dict(zip(keys, contact))
+        return jsonify(data), 200
+    else:
+        return jsonify({'error': 'Contact not found'}), 404
