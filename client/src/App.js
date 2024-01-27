c95e8d812d62927fcd3af3390bf8886d4c845073
sophiexie <sophiexie118@gmail.com>
2024-01-27 12:37:50 -0500

    Added router and routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,42 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import routes from "./routes"
+import './App.css';
 
 function App() {
-	const [data, setData] = useState("");
+    const [data, setData] = useState("");
 
 	// Using useEffect for single rendering (example of how to get backend data)
-	useEffect(() => {
-    fetch('http://localhost:5000/test', {
-      method : "GET"
-    })
-    .then(function (response) {
-      return response.json();
-  })
-    .then(function (bodyText) {
-        setData(bodyText.name);
-    })
-	}, []);
+    useEffect(() => {
+        fetch('http://localhost:5000/test', {
+            method : "GET"
+        })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (bodyText) {
+                setData(bodyText.name);
+            })
+    }, []);
 
-	return (
-		<div className="App">
-      <h1>React and flask</h1>
-      <p>{data}</p>
+    return (
+        <div className="App">
+            <p>{data}</p>
 
-		</div>
-	);
+            <Router>
+                <Routes>
+                    {routes.map((route, index) =>
+                            <Route key={index}
+                                   path={route.path}
+                                   element={route.element}
+                            />
+
+                    )}
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,24 @@
-function Home() {
-  return (
-    <div>
-      Home page
-    </div>
-  );
-}
+import React from 'react';
+import ContactCard from "../components/contactCard";
 
-export default Home;
+export default function Home() {
+    const tempContact = {
+        first_name: 'Sophie',
+        last_name: 'Xie',
+        email: 'sophie@gmail.com',
+        phone: '9056163560',
+        company: 'XYC Inc',
+        position: 'Sales Rep',
+        location: 'Toronto, ON, Canada',
+        notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim',
+        meeting_history: ['02-02-2028', '01-01-2027'],
+        labels: ['Potential Customer', 'Contact Soon'],
+        contact_frequency: 7,
+    }
+
+    return(
+        <div>
+            <ContactCard contact={tempContact}/>
+        </div>
+    )
+}
