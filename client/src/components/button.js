0189f655e7556fd2386c321ef843bb19ac351085
sophiexie <sophiexie118@gmail.com>
2024-01-27 16:01:53 -0500

    Added button

---
+++ client/src/components/button.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './button.css'
+
+export default function Button({
+                                   onClick,
+                                   text = 'Button',
+                                   color = '#00527A',
+                                   fontSize=16,
+                                   fontWeight='400',
+                                   background = 'white',
+                                   border = '#C3C0C0 3px solid',
+                                   hover,
+                                   width='fit-content',
+                                   height='fit-content',
+                                   margin='5px',
+                                   padding = '5px 10px 5px 10px',
+                                   disabled = false,
+                               }) {
+
+    return (
+        <button
+            onClick={disabled ? () => {} : onClick}
+            disabled={disabled}
+            style={{color: color, fontSize: fontSize, fontWeight: fontWeight, background: background, border: border, width: width, height: height, padding: padding, margin:margin}}
+        >
+            {text}
+        </button>
+    );
+}
