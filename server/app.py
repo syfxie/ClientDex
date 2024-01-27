1b78f4da9bcc5d029971a81784c1f3ef8e0d5266
Lorena Rosati <lrosati@outlook.com>
2024-01-27 15:01:07 -0500

    mongodb set up, delete endpoint

--- server/app.py
+++ server/app.py
@@ -1,14 +1,18 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from flask_cors import CORS
+from datetime import datetime, timezone
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 CORS(app)
 
-client = MongoClient('localhost', 27017)
+CONNECTION_STRING = "mongodb+srv://uoft:oPqKe1qnlgxLVJQF@cluster0.fjluu9a.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(CONNECTION_STRING)
 
-db = client.flask_db
-contacts = db.contacts
+dbname = client['contacts']
+contacts = dbname["contacts"]
 
 # Test route
 @app.route('/test', methods=['GET'])
@@ -20,7 +24,8 @@ def create_contact():
     data = request.get_json()
     app.logger.info(data)
 
-    # add to database
+    contacts.insert_one(data)
+    # check for invalid input
 
     response = {'message': 'Contact added successfully'}
     return jsonify(response), 201
@@ -78,20 +83,37 @@ def update_contact():
         return jsonify(data), 200
     else:
         return jsonify({'error': 'Contact not found'}), 404
+    
+# @app.route('/update_contact/add_meeting', methods=['PUT'])
+# def add_meeting():
+#     data = request.get_json()
+#     app.logger.info(data)
+
+#     # fix
+#     try:
+#         filter = {'_id': data['_id']}
+#         newdata = { "$set": { "lastContacted" : datetime.datetime.now(tz=datetime.timezone.utc) } }
+#         contacts.update_one(filter, newdata)
+#         response = {'message': 'Last meeting time updated'}
+#         return jsonify(response), 200
+#     except Exception as e:
+#         response = {'message': e}
+#         return jsonify(response), 400 
 
 
 @app.route('/delete', methods=['DELETE'])
 def delete_contact():
     data = request.get_json()
-    contact_id = data['id']
+    contact_id = data['_id']
 
     if not (contact_id and len(contact_id) > 0):
         return jsonify({'error': 'Invalid contact ID'}), 400
 
-    # remove from database
-    success = True
-
-    if success:
-        return jsonify(data), 200
-    else:
-        return jsonify({'error': 'Contact not found'}), 404
+    try:
+        result = contacts.delete_one({'_id': ObjectId(contact_id)})
+        if result.deleted_count == 1:
+            return jsonify({'message': 'Deleted contact'}), 200
+        else:
+            return jsonify({'message': 'Contact not found'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
